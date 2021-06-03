95e0d175b5b79c9556dba0bf605ce1cb9ff9ad9d
seanpgillick <75447516+seanpgillick@users.noreply.github.com>
2021-06-03 17:27:40 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ app.use(express.static("public_html"));
 ///////////Database/////////
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
+/*var connection = mysql.createConnection({
     host     : process.env.RDS_HOSTNAME,
     user     : process.env.RDS_USERNAME,
     password : process.env.RDS_PASSWORD,
     port     : process.env.RDS_PORT
-    /*host     : 'aawpkx9tfs32a9.chke9dfcgba6.us-east-2.rds.amazonaws.com',
+    host     : 'aawpkx9tfs32a9.chke9dfcgba6.us-east-2.rds.amazonaws.com',
     user     : 'admin',
-    password : 'p4ssw0rd123'*/
+    password : 'p4ssw0rd123'
 
 });
 
@@ -26,7 +26,7 @@ connection.connect(function(err) {
     }
 
     console.log('Connected to database.');
-});
+});*/
 
 app.post("/login", function (req, res) {
     console.log(connection);

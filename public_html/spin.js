9046a39283447e729a6d63cf99884827feb89d4f
GrantFecteau <grant.pf@gmail.com>
2021-06-03 16:48:54 -0400

    Update spin.js

--- public_html/spin.js
+++ public_html/spin.js
@@ -150,22 +150,22 @@ function createTable(rows, cols) {
 function makeImages(){
     let iTen = document.createElement("img");
     iTen.src = "symbols/ten.png";
-    iTen.dataValue = 1;
+    iTen.dataValue = "1";
     iTen.id = "ten";
 
     let iPlum = document.createElement("img");
     iPlum.src = "symbols/plum.png";
-    iPlum.dataValue = 2;
+    iPlum.dataValue = "2";
     iPlum.id = "plum";
 
     let iLemon = document.createElement("img");
     iLemon.src = "symbols/lemon.png";
-    iLemon.dataValue = 3;
+    iLemon.dataValue = "3";
     iLemon.id = "lemon";
 
     let iDiamond = document.createElement("img");
     iDiamond.src = "symbols/diamond.png";
-    iDiamond.dataValue = 4;
+    iDiamond.dataValue = "4";
     iDiamond.id = "diamond";
 
 }

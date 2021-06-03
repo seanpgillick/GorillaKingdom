22d095a1805aebb542b194342ce02e6c841a67fc
GrantFecteau <grant.pf@gmail.com>
2021-06-03 18:03:59 -0400

    Update server.js

--- server.js
+++ server.js
@@ -269,5 +269,5 @@ function checkDr(arr){
     return ((arr[0][0] === arr[1][1]) && (arr[1][1] === arr[2][2]));
 }
 function checkDl(arr){
-    return (arr[0][2] === arr[1][1] === arr[2][0]);
+    return ((arr[0][2] === arr[1][1]) && (arr[1][1] === arr[2][0]));
 }

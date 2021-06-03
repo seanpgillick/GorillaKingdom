3bf96612a9585f541f0c0ce455ddeb45a7304eac
GrantFecteau <grant.pf@gmail.com>
2021-06-03 17:01:01 -0400

    Update spin.js

--- public_html/spin.js
+++ public_html/spin.js
@@ -129,22 +129,22 @@ spin.addEventListener("click", function(){
 function findImg(num) {
     let iTen = document.createElement("img");
     iTen.src = "symbols/ten.png";
-    iTen.dataValue = "1";
+    iTen.dataValue = 1;
     iTen.id = "ten";
 
     let iPlum = document.createElement("img");
     iPlum.src = "symbols/plum.png";
-    iPlum.dataValue = "2";
+    iPlum.dataValue = 2;
     iPlum.id = "plum";
 
     let iLemon = document.createElement("img");
     iLemon.src = "symbols/lemon.png";
-    iLemon.dataValue = "3";
+    iLemon.dataValue = 3;
     iLemon.id = "lemon";
 
     let iDiamond = document.createElement("img");
     iDiamond.src = "symbols/diamond.png";
-    iDiamond.dataValue = "4";
+    iDiamond.dataValue = 4;
     iDiamond.id = "diamond";
     
     if (num === iTen.getAttribute("data-value")) {

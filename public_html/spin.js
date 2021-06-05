bab0eb3086cab134db4b3f6d490883bd81ef0eb8
GrantFecteau <grant.pf@gmail.com>
2021-06-05 16:25:10 -0400

    Update spin.js

--- public_html/spin.js
+++ public_html/spin.js
@@ -127,7 +127,7 @@ spin.addEventListener("click", function(){
             console.log(data)
             document.getElementById("payout").innerText = data.payout;
             if(data.payout > 0) {
-                playerbal = parseInt(playerbal + parseInt(data.payout)).toFixed(2);
+                playerbal = playerbal + parseInt(data.payout);
                 for(var x = 0; x < 10; x++){
                     for(var i = 0; i < data.lines.length; i++){
                         let symbol = document.getElementById(data.lines[i]).firstChild;

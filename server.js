027110a444c1c25477aaa1663e47ecc661b0ec2e
GrantFecteau <grant.pf@gmail.com>
2021-06-07 22:31:22 -0400

    Update server.js

--- server.js
+++ server.js
@@ -325,7 +325,7 @@ app.post("/spin", function(req, res) {
             betAmount = parseFloat(req.body.bet);
                         
             if (betAmount > dbBalance){
-                res.status(403).send({"validBet":false}); 
+                res.send({"validBet": false}); 
             }
         }
     });
